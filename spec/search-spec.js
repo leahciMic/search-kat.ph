942fd7feb1c04282be4393e311c2a59b599f58c5
Michael Leaney <mleaney@tokbox.com>
2015-12-02 11:01:02 +1100

    Increase jasmine timeout for slow responses

--- spec/search-spec.js
+++ spec/search-spec.js
@@ -1,5 +1,7 @@
 var search = require('../search.js');
 
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+
 describe('Parse kat.ph results', function() {
     it('should get something sane from a search', function(done) {
         search('test').then(function(results) {
